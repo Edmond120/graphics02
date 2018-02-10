dfe0f1ea2416b88f01195e28cb2329a48ba0e38e
edmond120 <edmondwong888@msn.com>
2018-02-09 20:42:31 -0500

    done

--- draw.py
+++ draw.py
@@ -10,9 +10,17 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 		y1 = tmp
 	A = y1 - y0
 	B = -1 * (x1 - x0)
+	if(B == 0):
+		if(y0 < y1):
+			for y in xrange(y0,y1):
+				plot(screen,color,x0,y)
+		else:
+			for y in xrange(y1,y0):
+				plot(screen,color,x0,y)
+		return
 	slope = A / (B * -1.0) #todo check for divide by 0
 	C = y1 - x1 * slope
-	if(slope > 0 and slope <= 1): #octant 1
+	if(slope >= 0 and slope <= 1): #octant 1
 		d = 2 * A + B
 		while x0 <= x1:
 			plot(screen,color,x0,y0)
@@ -32,7 +40,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 			y0 += 1
 			d -= 2 * B
 		return
-	elif(slope < 0 and slope <= -1):
+	elif(slope < 0 and slope >= -1):
 		d = 2 * A + B
 		while x0 <= x1:
 			plot(screen,color,x0,y0)
@@ -44,11 +52,11 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 		return
 	elif(slope < -1):
 		d = A + 2 * B
-		while y0 <= y1:
+		while abs(y0) >= abs(y1):
 			plot(screen,color,x0,y0)
 			if(d > 0):
 				x0 += 1
 				d += 2 * A
 			y0 -= 1
-			d += 2 * B
+			d -= 2 * B
 		return
